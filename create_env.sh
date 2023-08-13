58b8a0582b7346973dbc59277d19908859fa26bf
gh-fatima <78655461+gh-fatima@users.noreply.github.com>
2023-08-13 17:41:46 -0400

    Update create_env.sh

--- create_env.sh
+++ create_env.sh
@@ -1,15 +1,15 @@
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh 
-koli Enter
-yes
-Enter
-yes
-new terminal 
+# koli Enter
+# yes
+# Enter
+# yes
+# new terminal 
 conda create -n Sup python=3.9
-y
+# y
 conda activate Sup
 conda install cudatoolkit=11.7 -c nvidia
-y
+# y
 pip install torch
 pip install torchvision
 pip install matplotlib
