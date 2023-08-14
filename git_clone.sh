51734779df2607666b40cadeed43db89fc06af1d
gh-fatima <78655461+gh-fatima@users.noreply.github.com>
2023-08-14 19:10:08 -0400

    Delete git_clone.sh

--- git_clone.sh
+++
@@ -1,8 +0,0 @@
-sudo apt-get install git
-git clone https://github.com/gh-fatima/chameleon-setup.git
-cd chameleon-setup
-bash install_gcloud.sh
-cd ..
-git clone https://github.com/gh-fatima/One-Epoch.git
-gcloud auth login
-gsutil cp gs://advml/logs.zip ~/One-Epoch
