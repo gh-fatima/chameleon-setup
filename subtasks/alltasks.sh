c0d281b3c09b2c1319f492c7550817289d670e6a
gh-fatima <78655461+gh-fatima@users.noreply.github.com>
2023-08-14 15:32:28 -0400

    Update alltasks.sh

--- subtasks/alltasks.sh
+++ subtasks/alltasks.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-# Cloaning nessessery files from my github
-function install_custom_mlserver() {
-    echo "Installing the customized MLServer"
-    cd ~/ipa/MLServer
-    git checkout configure-custom-1
-    make install-dev
+# Cloning nessessery files from my github
+function git_clone() {
+    echo "cloning files"
+    udo apt-get install git
+    git clone https://github.com/gh-fatima/chameleon-setup.git
+    cd chameleon-setup
+    bash install_gcloud.sh
     cd ..
-    echo "MLServer installation complete"
+    git clone https://github.com/gh-fatima/One-Epoch.git
+    gcloud auth login
+    gsutil cp gs://advml/logs.zip ~/One-Epoch
     echo
 }
 
@@ -33,97 +36,6 @@ function install_conda_environment() {
 }
 
 
-#!/bin/bash
-
-# Update and install necessary packages
-function install_packages() {
-    echo "Updating packages"
-    sudo apt-get update -y
-    echo "Installing packages"
-    sudo apt-get install ffmpeg libsm6 libxext6 -y
-    echo "Packages installation complete"
-    echo
-}
-
-# Install and activate Conda environment
-function install_conda_environment() {
-    echo "Removing Conda environment"
-    conda deactivate
-    conda env remove --name central
-
-    echo "Installing and activating Conda environment"
-    wget https://repo.anaconda.com/miniconda/Miniconda3-py39_23.3.1-0-Linux-x86_64.sh -O miniconda-39.sh
-    bash miniconda-39.sh -fb
-    rm miniconda-39.sh
-
-    source ~/miniconda3/etc/profile.d/conda.sh
-    conda init --all
-    conda create --name central -y python=3.9.15
-    conda activate central
-    echo "Conda environment installation complete"
-    echo
-}
-
-# Install customized MLServer
-function install_custom_mlserver() {
-    echo "Installing the customized MLServer"
-    cd ~/ipa/MLServer
-    git checkout configure-custom-1
-    make install-dev
-    cd ..
-    echo "MLServer installation complete"
-    echo
-}
-
-# Install ipa requirements
-function install_inference_pipeline() {
-    echo "Installing ipa requirements"
-    cd ~/ipa
-    pip install -r requirements.txt
-    cd ..
-    echo "ipa requirements installation complete"
-    echo
-}
-
-# Install load_tester
-function install_load_tester() {
-    echo "Installing load tester"
-    cd ~/ipa/load_tester
-    pip install -e .
-    cd ..
-    echo "load_tester installation complete"
-    echo
-}
-
 # Call the functions
-install_packages
+git_clone
 install_conda_environment
-install_custom_mlserver
-install_inference_pipeline
-install_load_tester
-# Install ipa requirements
-function install_inference_pipeline() {
-    echo "Installing ipa requirements"
-    cd ~/ipa
-    pip install -r requirements.txt
-    cd ..
-    echo "ipa requirements installation complete"
-    echo
-}
-
-# Install load_tester
-function install_load_tester() {
-    echo "Installing load tester"
-    cd ~/ipa/load_tester
-    pip install -e .
-    cd ..
-    echo "load_tester installation complete"
-    echo
-}
-
-# Call the functions
-install_packages
-install_conda_environment
-install_custom_mlserver
-install_inference_pipeline
-install_load_tester
